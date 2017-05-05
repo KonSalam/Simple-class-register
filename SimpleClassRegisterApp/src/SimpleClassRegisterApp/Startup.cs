82421ef4214ce7cf81643d48611e5d7d91ad4cff
KonSalam <konradsalamon95@gmail.com>
2017-05-06 01:21:22 +0200

    Create project environment and routing configuration

--- SimpleClassRegisterApp/src/SimpleClassRegisterApp/Startup.cs
+++ SimpleClassRegisterApp/src/SimpleClassRegisterApp/Startup.cs
@@ -7,30 +7,56 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace SimpleClassRegisterApp
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        private readonly IConfigurationRoot configuration;
+
+        public Startup(IHostingEnvironment env)
+        {
+            configuration = new ConfigurationBuilder()
+                                .AddEnvironmentVariables()
+                                .AddJsonFile(env.ContentRootPath + "/config.json")
+                                .AddJsonFile(env.ContentRootPath + "/config.development.json", true)
+                                .Build();
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
+            services.AddTransient<FeatureToggles>(x => new FeatureToggles
+            {
+                EnableDeveloperExceptions =
+                   configuration.GetValue<bool>("FeatureToggles:EnableDeveloperExceptions")
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, FeatureToggles features)
         {
             loggerFactory.AddConsole();
+            app.UseExceptionHandler("/error.html");
 
-            if (env.IsDevelopment())
+            if (features.EnableDeveloperExceptions)
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.Use(async (context, next) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                if (context.Request.Path.Value.Contains("invalid"))
+                    throw new Exception("ERROR!");
+
+                await next();
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute("Default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
