4ee3a31da725988187435b5f440ed5c2ef606176
KonSalam <konradsalamon95@gmail.com>
2017-05-16 23:11:17 +0200

    Add Register Form

---
+++ SimpleClassRegisterApp/src/SimpleClassRegisterApp/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SimpleClassRegisterApp.Models.Account;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SimpleClassRegisterApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SignUp()
+        {
+            return View(new SignUpViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(SignUpViewModel register)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(new SignUpViewModel());
+            }
+
+            return RedirectToAction("Login", "Account");
+
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ SimpleClassRegisterApp/src/SimpleClassRegisterApp/Models/Account/SignUpViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleClassRegisterApp.Models.Account
+{
+    public class SignUpViewModel
+    {
+
+        [Required(ErrorMessage = "First Name is Required")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is Required")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress, MaxLength(500)]
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Passwords must match")]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
