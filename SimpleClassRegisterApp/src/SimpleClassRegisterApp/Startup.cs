c61bad855a716d3f9c2ca99c0a4c1e5f39e8c176
KonSalam <konradsalamon95@gmail.com>
2017-06-10 12:23:37 +0200

    Add Logger Exception to File and Change checked classes color

--- SimpleClassRegisterApp/src/SimpleClassRegisterApp/Startup.cs
+++ SimpleClassRegisterApp/src/SimpleClassRegisterApp/Startup.cs
@@ -71,6 +71,7 @@ namespace SimpleClassRegisterApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, FeatureToggles features, RoleManager<IdentityRole> roleManager,ClassRegisterDataContext registerContext)
         {
+            loggerFactory.AddFile("Logs/mylog-{Date}.txt",LogLevel.Error);
             loggerFactory.AddConsole();
             app.UseExceptionHandler("/Home/Error");
 
